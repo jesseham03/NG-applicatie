1eb351a7f2da3621983f1feb93a5e3e2dc55cbb0
jesseham03 <90609756+jesseham03@users.noreply.github.com>
2022-04-26 14:54:57 +0200

    opschonen

--- src/main/java/Network.java
+++ src/main/java/Network.java
@@ -96,35 +96,5 @@ public class Network implements Serializable {
         return firewallAvailability * dataBaseAvailability * webServerAvailability;
     }
 
-    /*
-    public static void main(String[] args) {
-        Network network1 = new Network();
-        InfrastructureComponent webServer1 = new InfrastructureComponent("naam1", 20, 0.9f, InfrastructureComponent.Type.Web);
-        InfrastructureComponent webServer2 = new InfrastructureComponent("naam1", 20, 0.9f, InfrastructureComponent.Type.Web);
-        InfrastructureComponent webServer3 = new InfrastructureComponent("naam1", 20, 0.9f, InfrastructureComponent.Type.Web);
-        InfrastructureComponent webServer4 = new InfrastructureComponent("naam1", 20, 0.95f, InfrastructureComponent.Type.Web);
-        InfrastructureComponent dataBase1 = new InfrastructureComponent("naam1", 20, 0.8f, InfrastructureComponent.Type.Database);
-        InfrastructureComponent database2 = new InfrastructureComponent("naam1", 20, 0.8f, InfrastructureComponent.Type.Database);
-        InfrastructureComponent database3 = new InfrastructureComponent("naam1", 20, 0.8f, InfrastructureComponent.Type.Database);
-        InfrastructureComponent database4 = new InfrastructureComponent("naam1", 20, 0.8f, InfrastructureComponent.Type.Database);
-        InfrastructureComponent database5 = new InfrastructureComponent("naam1", 20, 0.8f, InfrastructureComponent.Type.Database);
-        InfrastructureComponent database6 = new InfrastructureComponent("naam1", 20, 0.8f, InfrastructureComponent.Type.Database);
-        InfrastructureComponent database7 = new InfrastructureComponent("naam1", 20, 0.8f, InfrastructureComponent.Type.Database);
-        InfrastructureComponent fireWall1 = new InfrastructureComponent("naam1", 20, 0.99998f, InfrastructureComponent.Type.Firewall);
-        network1.addComponent(webServer1);
-        network1.addComponent(webServer2);
-        network1.addComponent(webServer3);
-        network1.addComponent(webServer4);
-        network1.addComponent(dataBase1);
-        network1.addComponent(database2);
-        network1.addComponent(database3);
-        network1.addComponent(database4);
-        network1.addComponent(database5);
-        network1.addComponent(database6);
-        network1.addComponent(database7);
-        network1.addComponent(fireWall1);
-        System.out.println(network1.calculateAvailability());
-        System.out.println(network1.calculatePrice());
-    }
-     */
+
 }
